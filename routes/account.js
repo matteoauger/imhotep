57b2b6192942c500cea98d36b0d310a1818db06b
Matteo AUGER <augermtt@gmail.com>
2019-10-24 09:56:03 +0200

    Rename login.js to account.js

--- routes/account.js
+++ routes/account.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const User = require('../model/user-schema');
+const USER_ROLES = require('../model/user-roles');
 
 router.get('/login', (req, res) => {
-  res.render('user/login', {error: null, data: null});
+  res.render('account/login', {error: null, data: null});
 });
 
 router.post('/login', (req, res) => {
@@ -15,38 +16,39 @@ router.post('/login', (req, res) => {
         req.session.userId = user._id;
         res.redirect('/');
       })
-      .catch(_ => res.render('user/login', { error: 'Identifiants invalides', data: req.body }));
+      .catch(_ => res.render('account/login', { error: 'Identifiants invalides', data: req.body }));
   } else {
-    res.render('user/login', { error: 'Merci de préciser l\'email et le mot de passe', data: req.body });
+    res.render('account/login', { error: 'Merci de préciser l\'email et le mot de passe', data: req.body });
   }
 });
 
 router.get('/register', (req, res) => {
-  res.render('user/register', { data: null, error: ""  });
+  res.render('account/register', { data: null, error: ""  });
 });
 
 router.post('/register', (req, res) => {
   if (req.body.email && 
       req.body.firstname && 
-      req.body.lastname && 
+      req.body.lastname &&
       req.body.password) {
       // inserting the unser into the database
       insertUser(req)
         .then(user => {
           // storing the id into the session
           req.session.userId = user._id;
+          req.session.roleId = user.role_id;
           res.redirect('/');
         })
-        .catch(_ => 
-          res.render('user/register', { title: 'Créer un compte', data: req.body, error: `Le compte associé à ${req.body.email} existe déjà.` })
+        .catch(err =>
+          res.render('account/register', { data: req.body, error: `Le compte associé à ${req.body.email} existe déjà.` })
         );
   } else {
-    res.render('user/register', { title: 'Créer un compte', data: req.body, error: "Merci de renseigner la totalité des champs." });
+    res.render('account/register', { data: req.body, error: "Merci de renseigner la totalité des champs." });
   }
 });
 
 router.get('/logout', (req, res) => {
-  if (req.session && req.session.userId) {
+  if (req.session) {
     req.session.destroy(_ => res.redirect('/'));
   } else {
     res.redirect('/');
@@ -62,6 +64,8 @@ async function insertUser(req) {
   userData.email = body.email;
   userData.lastname = body.lastname;
   userData.password = hashedPassword;
+  // setting the role to user by default
+  userData.role_id  = USER_ROLES.user.id;
 
   // inserting the user into the database
   await userData.save();
