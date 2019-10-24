17cf1c44cbaae49c73098fb6352c47c3b757d629
Matteo AUGER <augermtt@gmail.com>
2019-10-24 13:47:27 +0200

    Fix role restriction middleware

--- middleware/role-restriction.js
+++ middleware/role-restriction.js
@@ -1,12 +1,13 @@
-function roleRestriction(roleId) {
+function roleRestriction(role) {
     return function(req, res, next) {
-        if (req.session && req.session.roleId && req.session.roleId <= roleId) {
+        if (req.session && req.session.roleId <= role.id) {
             return next();
         } else {
+            const err = new Error('Vous ne disposez pas des droits nécessaires pour accéder à cette page ');
             err.status = 401;
             return next(err);
         }
     }
 }
 
-module.exports = requiresLogin;
+module.exports = roleRestriction;
